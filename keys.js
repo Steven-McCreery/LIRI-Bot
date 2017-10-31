891216576423fcd2eb58b786d835adfb8b5344d0
Steven McCreery <logad8@gmail.com>
2017-10-31 18:14:41 -0400

    added beginning of if/function calling mechanism, and adjusted the keys and random.txt files.

--- keys.js
+++ keys.js
@@ -1,12 +1,13 @@
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
+console.log('this is loaded');
 
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
 
-
+module.exports = twitterKeys;
 
 
 
